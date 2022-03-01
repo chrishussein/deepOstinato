429f81320599ec34c8f343ec879dd6770865cb35
Viviane Simolka <viviane.thomas@gmail.com>
2022-03-01 15:20:25 +0100

    Updated padder class to work in a pipeline

--- deepOstinato/preprocessing/pad.py
+++ deepOstinato/preprocessing/pad.py
@@ -1,15 +1,15 @@
 import librosa as lr
+from sklearn.base import BaseEstimator, TransformerMixin
 
-class Pad:
+class Padder(BaseEstimator, TransformerMixin):
     """Pads audio, pad_size corresponds to total length of audio series"""
     def __init__(self, pad_size=1500000):
         self.pad_size = pad_size
 
+    def fit(self):
+        """Fit method for the padder"""
+        return self
 
-    def pad(self, audio):
-        padded_audio_pieces = []
-        for piece in audio:
-            padded_audio_piece = lr.util.fix_length(piece, self.pad_size)
-
-            padded_audio_pieces.append(padded_audio_piece)
-        return padded_audio_pieces
+    def transform(self, audio):
+        """Transform method to pad the audio file"""
+        return [lr.util.fix_length(piece, self.pad_size) for piece in audio]
