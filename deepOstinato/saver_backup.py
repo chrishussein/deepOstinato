f439487320d888895d361054275936159090fc81
Nicolas Andrews <n.andrews.jpiano@gmail.com>
2022-03-01 12:30:39 +0100

    working on reverse processing

--- deepOstinato/saver_backup.py
+++ deepOstinato/saver_backup.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class Saver:
+class npy_file_save:
     def __init__(self):
         pass
 
@@ -9,3 +9,5 @@ class Saver:
         for npy in audio:
             np.save(f'raw_data/transformed_audio/npy_file_no{counter}', npy)
             counter += 1
+
+class wav_file_save:
