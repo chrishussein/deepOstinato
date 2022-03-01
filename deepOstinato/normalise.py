68c9cab49cbf73f1dcee26fc03a32f5c8f506bb3
Viviane Simolka <viviane.thomas@gmail.com>
2022-03-01 10:04:25 +0100

    corrected normalizer and added inheritance

--- deepOstinato/normalise.py
+++ deepOstinato/normalise.py
@@ -1,13 +1,15 @@
-class MinMaxNormaliser:
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class MinMaxNormaliser(BaseEstimator, TransformerMixin):
     """Applis MinMaxNormalisation to an array.
     Takes minimum and maximum values from transformed audio"""
     def __init__(self, min_val, max_val):
-        self.min = min.val
+        self.min = min_val
         self.max = max_val
 
     def normalise(self, array):
         normalised_audio = (array - array.min()) / (array.max() - array.min())
-        normalised_audio = normalised_audio * (self.max  - self.min) = self.min
+        normalised_audio = normalised_audio * (self.max  - self.min) + self.min
         return normalised_audio
 
     def denormalise(self, normalised_audio, original_min, original_max):
