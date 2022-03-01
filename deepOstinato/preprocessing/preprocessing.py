c8e46a645e76ac323edc3aab558f4699a60090b8
Viviane Simolka <viviane.thomas@gmail.com>
2022-03-01 15:32:17 +0100

    preproc pseudo code

---
+++ deepOstinato/preprocessing/preprocessing.py
@@ -0,0 +1,14 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+#Gathering all preprocessing
+
+#get midi file, convert it to audio file.
+# Get audio files, filters it, performed the fourier transform, plot and save spectogram
+class Preprocesser(BaseEstimator, TransformerMixin):
+    """Preprocessing steps to convert """
+    def __init__(self):
+        pass
+
+    #get audio files
+
+    #filter audio files
